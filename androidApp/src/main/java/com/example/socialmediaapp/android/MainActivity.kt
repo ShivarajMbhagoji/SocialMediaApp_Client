aea243a6effd2634d06aadb0694e84f97b14ecec
ShivarajMBhagoji <bshivm135@gmail.com>
2024-08-06 11:45:58 +0530

    SplashScreen added.

--- androidApp/src/main/java/com/example/socialmediaapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/MainActivity.kt
@@ -6,14 +6,42 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainActivityViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
+        var uiState: MainActivityUiState by mutableStateOf(MainActivityUiState.Loading)
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }
+                    .collect()
+            }
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            uiState == MainActivityUiState.Loading
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
@@ -21,8 +49,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val authState = viewModel.authState.collectAsStateWithLifecycle(null)
-                     SocialApp(authState.value)
+                     SocialApp(uiState)
                 }
             }
         }
